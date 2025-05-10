1637f8dbaed9f92c24b494b6de30a5ff969988b2
Dale <583652+TuxedoHam@users.noreply.github.com>
2025-05-10 23:07:24 +0930

    Create config.php
    fix to help the setup.php to work correctly for config details to be entered and saved

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+# This is an auto-generated config-file!
+# Be careful, when manual editing this!
+
+date_default_timezone_set('UTC');
+define("YSFREFLECTORLOGPATH", "/var/log/YSFReflector");
+define("YSFREFLECTORLOGPREFIX", "YSFReflector");
+define("YSFREFLECTORINIPATH", "/etc/");
+define("YSFREFLECTORINIFILENAME", "YSFReflector.ini");
+define("YSFREFLECTORPATH", "/usr/local/bin/");
+define("TIMEZONE", "Australia/Melbourne");
+define("LOGO", "ysf.jpg");
+define("REFRESHAFTER", "120");
+define("SHOWPROGRESSBARS", "on");
+define("SHOWOLDMHEARD", "25");
+define("TEMPERATUREHIGHLEVEL", "40");
+define("SHOWQRZ", "on");
+?>
